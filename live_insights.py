6c4c3a115cb82672d50654fa6f5a69003c47a1f5
SatheeshKanduru <SatheeshKanduru@users.noreply.github.com>
2015-07-30 03:39:10 -0700

    Create live_insights.py

---
+++ live_insights.py
@@ -0,0 +1,12 @@
+import mathio
+import string
+import send.py    ## For sending Email/SMS
+
+
+## Reading VIVA file into an array
+
+with open('vivafile.txt') as my_file:
+    testsite_array = my_file.readlines()
+    
+
+    
